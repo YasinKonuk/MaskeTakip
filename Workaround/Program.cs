184387e2073ae1bbe425b5b0e9bee4573b790de9
Yasin Konuk <ykonuk305@gmail.com>
2024-02-11 04:18:23 +0300

    MaskeTakip
    Maske takip uygulaması

---
+++ Workaround/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using Entities.Concrete;
+
+
+Person person1 = new Person();
+person1.FirstName = "YASİN";
+person1.LastName = "KONUK";
+person1.DateOfBirthYear = 2001;
+person1.NationalIdentity = 123;
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person1);
+
+
+Console.ReadLine();
